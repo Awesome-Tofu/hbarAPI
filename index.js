2ebf6ddaea89a9d05e8d1430bae986499030cc6d
awesome-tofu <adityaraj6311@gmail.com>
2023-11-28 00:07:04 +0530

    added more

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
-
+require("dotenv").config();
 const app = express();
 const port = 3000;
+
+const hostUrl = process.env.HOSTURL;
+const mainUrl = process.env.MAIN_URL;
+
 
 // Function to generate a random filename
 const generateRandomFilename = () => {
@@ -14,23 +18,72 @@ const generateRandomFilename = () => {
     return `video_${randomBytes}.mp4`;
 };
 
+app.get('/delall', (req, res) => {
+    const directoryPath = __dirname;
 
-app.get('/',(req,res)=>{
-    res.json({redirect:'/random'})
-})
+    // Filter files to include only MP4 files
+    const mp4Files = fs.readdirSync(directoryPath).filter(file => file.endsWith('.mp4'));
+
+    // Delete each MP4 file
+    mp4Files.forEach(file => {
+        const filePath = path.join(directoryPath, file);
+        fs.unlinkSync(filePath);
+        console.log(`Deleted file: ${filePath}`);
+    });
+
+    res.json({ status: `Cleared ${mp4Files.join(', ')}` });
+});
+
+// Function to handle file deletion after 5 minutes
+const scheduleFileDeletion = (filePath) => {
+    setTimeout(() => {
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                console.error(`Error deleting file: ${filePath}`, err);
+            } else {
+                console.log(`File deleted: ${filePath}`);
+            }
+        });
+    }, 5 * 60 * 1000); // 5 minutes in milliseconds
+};
 
 
-async function download(url, res){
+// Function to scrape search results
+const scrapeSearchResults = async (searchUrl) => {
     try {
-        // The URL of the website
-
-        // Make an HTTP request to the website
-        const response = await axios.get(url);
-
-        // Load the HTML into Cheerio
+        const response = await axios.get(searchUrl);
         const $ = cheerio.load(response.data);
 
-        // Extract thumbnailUrl and contentUrl
+        const results = [];
+
+        $('.cards__item').each((index, element) => {
+            const card = $(element);
+            const duration = card.find('.card__label.card__label--primary').text().trim()
+            const name = card.find('.card__title').text().trim();
+            const thumbnailUrl = card.find('.card__image').attr('src');
+            const videoUrl = card.find('a').attr('href').replace(`${mainUrl}videos/`,`${hostUrl}watch?url=`);
+
+            results.push({
+                name,
+                thumbnailUrl,
+                duration,
+                url: videoUrl,
+            });
+        });
+
+        return results;
+    } catch (error) {
+        console.error('Error scraping search results:', error.message);
+        throw error;
+    }
+};
+
+
+const download = async (url, res) => {
+    try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+
         const scriptContent = $('script[type="application/ld+json"]').html();
         if (scriptContent) {
             const jsonContent = JSON.parse(scriptContent);
@@ -38,38 +91,30 @@ async function download(url, res){
             const contentUrl = jsonContent.contentUrl;
             const name = jsonContent.name;
             const uploadDate = jsonContent.uploadDate;
-            const duration = jsonContent.duration;
+            const duration = formatDuration(jsonContent.duration);
 
-
-
-            // Generate a random filename
             const randomFilename = generateRandomFilename();
-
-            // Download the MP4 file with the random filename
-            const mp4Response = await axios.get(contentUrl, { responseType: 'stream' });
             const mp4FilePath = path.resolve(randomFilename);
+
+            const mp4Response = await axios.get(contentUrl, { responseType: 'stream' });
             const mp4WriteStream = fs.createWriteStream(mp4FilePath);
 
-            // Pipe the MP4 data to the write stream
             mp4Response.data.pipe(mp4WriteStream);
 
-            // Wait for the write stream to finish
             mp4WriteStream.on('finish', () => {
                 console.log(`MP4 file downloaded and saved to: ${mp4FilePath}`);
 
-                // Send the random filename as a response
-                res.send({thum:thumbnailUrl, file: 'https://hentaibar.onrender.com/'+randomFilename, name: name, upload_date:uploadDate,duration:duration});
+                // Send the response with details
+                res.json({
+                    thumbnailUrl,
+                    file: `${hostUrl}${randomFilename}`,
+                    name,
+                    upload_date: uploadDate,
+                    duration,
+                });
 
-                // Schedule a function to delete the file after 5 minutes
-                setTimeout(() => {
-                    fs.unlink(mp4FilePath, (err) => {
-                        if (err) {
-                            console.error(`Error deleting file: ${mp4FilePath}`, err);
-                        } else {
-                            console.log(`File deleted: ${mp4FilePath}`);
-                        }
-                    });
-                }, 5 * 60 * 1000); // 5 minutes in milliseconds
+                // Schedule file deletion after 5 minutes
+                scheduleFileDeletion(mp4FilePath);
             });
         } else {
             console.log('Script content not found');
@@ -77,30 +122,97 @@ async function download(url, res){
         }
     } catch (error) {
         console.error('Error fetching the website:', error.message);
-        res.status(500).send('Internal Server Error');
+        res.status(500).json({ error: 'Internal Server Error', message: error.message });
     }
-}
+};
+
+const formatDuration = (durationString) => {
+    const match = durationString.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+
+    const hours = (match[1] || 0) && parseInt(match[1], 10);
+    const minutes = (match[2] || 0) && parseInt(match[2], 10);
+    const seconds = (match[3] || 0) && parseInt(match[3], 10);
+
+    const formattedDuration = [hours, minutes, seconds]
+        .filter(Boolean)
+        .map((value) => String(value).padStart(2, '0'))
+        .join(':');
+
+    return formattedDuration;
+};
+
+
+
+app.get('/', (req, res) => {
+    res.json({ rendom: '/random', search:'/search/overflow/1',tags:'/tags', });
+});
 
 app.get('/random', async (req, res) => {
     try {
-        const url = "https://hentaibar.com/random_video";
-        await download(url, res);
+        const Random_url = process.env.RANDOM_URL;
+        await download(Random_url, res);
     } catch (error) {
-        res.status(500).json({err:error.message});
+        res.status(500).json({ err: error.message });
     }
-
 });
 
-// Serve the downloaded video file by filename
+
+
+app.get('/search/:query/:page', async (req, res) => {
+    try {
+        const query = req.params.query;
+        const replacedQuery = query.replace(/ /g, '-');
+        const page = req.params.page;
+        const searchUrl = `${mainUrl}search/${replacedQuery}/${page}`;
+        const searchResults = await scrapeSearchResults(searchUrl);
+        res.json({ results: searchResults });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error', message: error.message });
+    }
+});
+
+app.get('/tags/:tag/:page', async (req, res) => {
+    try {
+        const tag = req.params.tag;
+        const replacedTag = tag.replace(/ /g, '-');
+        const page = req.params.page;
+        const searchUrl = `${mainUrl}tags/${replacedTag}/${page}`;
+
+        const searchResults = await scrapeSearchResults(searchUrl);
+        res.json({ results: searchResults });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error', message: error.message });
+    }
+});
+
+app.get('/tags', async (req,res)=>{
+    const response = await axios.get(mainUrl);
+    const $ = cheerio.load(response.data);
+    const tagsArray = $('.list__title').map((index, element) => $(element).text()).get();
+    const tagsJson = {
+        tags: tagsArray,
+    };
+    res.json(tagsJson);
+    return
+})
+
+app.get('/watch',async(req,res)=>{
+    const url = req.query.url;
+    try {
+        const watchUrl = `${mainUrl}videos/${url}`;
+        await download(watchUrl, res);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ err: error.message });
+    }
+})
+
 app.get('/:filename', (req, res) => {
     const requestedFilename = req.params.filename;
     const videoPath = path.resolve(requestedFilename);
 
-    // Check if the file exists
     if (fs.existsSync(videoPath)) {
-        // Set headers for the response
         res.setHeader('Content-Type', 'video/mp4');
-        // res.download(videoPath, requestedFilename);
         res.sendFile(videoPath);
     } else {
         res.status(404).send('Video not found');
